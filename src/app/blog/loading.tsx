1e7e286235d86a2cb3a78d649a5054f3f4af9b79
siddiqua1997 <98895128+siddiqua1997@users.noreply.github.com>
2026-02-08 17:09:50 +0400

    ux: blog-only loading bar thicker and slower

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,7 @@
+export default function BlogLoading() {
+  return (
+    <div className="loading-bar loading-bar--blog">
+      <div className="loading-bar__inner" />
+    </div>
+  );
+}
--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function GlobalLoading() {
-  return (
-    <div className="loading-bar">
-      <div className="loading-bar__inner" />
-    </div>
-  );
-}
